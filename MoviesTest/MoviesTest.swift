4d6443998f7298d1341d2e467065b6e401aeaff3
Timofey Privalov <timofmax@gmail.com>
2021-10-14 21:09:12 +0300

    tests added

--- MoviesTest/MoviesTest.swift
+++ MoviesTest/MoviesTest.swift
@@ -9,26 +9,49 @@
 import UIKit
 import XCTest
 
+final class MockNavigationController: UINavigationController {
+    var presentedVC: UIViewController?
+
+    override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+        presentedVC = viewController
+        super.pushViewController(viewController, animated: animated)
+    }
+
+}
+
+
 class MoviesTest: XCTestCase {
+
+    var coordinator: ApplicationCoordinator!
+    var navController: MockNavigationController!
+    var assembly: AssemblyProtocol!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        navController = MockNavigationController()
+        assembly = Assembly()
+        coordinator = ApplicationCoordinator(assembly: assembly, navController: navController)
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        navController = nil
+        assembly = nil
+        assembly = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testMainViewControllerScreen() throws {
+        coordinator.start()
+        let movieVC = navController.presentedVC
+        XCTAssertTrue(movieVC is MovieViewController)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testDetailViewControllerScreen() {
+        coordinator.start()
+        guard let movieViewController = navController.presentedVC as? MovieViewController else { return }
+        movieViewController.toDetailScreen?(0)
+        let movieDetailViewController = navController.presentedVC
+        XCTAssertTrue(movieDetailViewController is MovieDetailViewController)
     }
 
 }
