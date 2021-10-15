2e8f7264c4b533ebc9993f3c46b31a0a45ca4ea8
Timofey Privalov <timofmax@gmail.com>
2021-10-15 18:41:52 +0300

    Realm *

--- NetworkingProject/DetailModule/Model/MovieDetails.swift
+++ NetworkingProject/DetailModule/Model/MovieDetails.swift
@@ -15,3 +15,42 @@ struct MovieDetails: Decodable {
     let releaseDate: String
     let revenue: Int
 }
+
+
+/*
+@objc final class MovieDetails: Object, Codable {
+    enum CodingKeys: String, CodingKey {
+        case id
+        case budget
+        case revenue
+        case overview
+        case backdropPath = "backdrop_path"
+        case originalTitle = "original_title"
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+    }
+
+    // MARK: - Public Properties
+
+    /// id фильма
+    @objc dynamic var id: Int
+    /// бюджет фильма
+    @objc dynamic var budget: Int
+    /// зарабаток фильма
+    @objc dynamic var revenue: Int
+    /// описание фильма
+    @objc dynamic var overview: String
+    /// путь к картинке фильма
+    @objc dynamic var backdropPath: String
+    /// оригинальное название фильма
+    @objc dynamic var originalTitle: String
+    /// путь к постеру фильма
+    @objc dynamic var posterPath: String
+    /// дата выхода фильма
+    @objc dynamic var releaseDate: String
+    /// уникальный ключ для дб
+    override class func primaryKey() -> String {
+        "id"
+    }
+}
+*/
