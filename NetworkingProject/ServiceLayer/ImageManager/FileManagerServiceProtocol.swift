dab491367b9497d51d73475305e127438a4c31e1
Timofey Privalov <timofmax@gmail.com>
2021-10-16 01:36:21 +0300

    FileManagerServiceProtocol

--- NetworkingProject/ServiceLayer/ImageManager/FileManagerServiceProtocol.swift
+++ NetworkingProject/ServiceLayer/ImageManager/FileManagerServiceProtocol.swift
@@ -14,13 +14,50 @@ protocol FileManagerServiceProtocol {
 }
 
 final class FinalManagerService: FileManagerServiceProtocol {
+    // MARK: - Private Properties
+    private var images = [String: UIImage]()
+    private let cacheLifeTime: TimeInterval = 30 * 24 * 60 * 60
+    private static let pathname: String = {
+        let pathName = "images"
+        guard let cacheDirectory = FileManager.default.urls(for: .cachesDirectory, in: .allDomainsMask).first
+        else { return pathName }
+
+        let url = cacheDirectory.appendingPathComponent(pathName, isDirectory: true)
+
+        if !FileManager.default.fileExists(atPath: url.path) {
+            try? FileManager.default.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
+        }
+        return pathName
+    }()
+
+    // MARK: - Public Methods
     func getImageFromCache(url: String) -> UIImage? {
-        return UIImage()
+        guard let fileName = getFilePath(url: url),
+              let info = try? FileManager.default.attributesOfItem(atPath: fileName),
+              let modificationDate = info[FileAttributeKey.modificationDate] as? Date
+        else { return nil }
+
+        let lifeTime = Date().timeIntervalSince(modificationDate)
+
+        guard lifeTime <= cacheLifeTime,
+              let image = UIImage(contentsOfFile: fileName) else { return nil }
+        DispatchQueue.main.async {
+            self.images[url] = image
+        }
+        return image
     }
 
     func saveImageToCache(url: String, image: UIImage) {
-        
+        guard let fileName = getFilePath(url: url),
+              let data = image.pngData() else { return }
+        FileManager.default.createFile(atPath: fileName, contents: data, attributes: nil)
     }
 
-
+    // MARK: - Private methods
+    private func getFilePath(url: String) -> String? {
+        guard let cacheDirectory = FileManager.default.urls(for: .cachesDirectory, in: .allDomainsMask).first
+        else { return nil }
+        let hashName = url.split(separator: "/").last ?? "default"
+        return cacheDirectory.appendingPathComponent(FinalManagerService.pathname + "/" + hashName).path
+    }
 }
