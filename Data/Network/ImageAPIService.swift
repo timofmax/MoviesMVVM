f473874d0f34958901e3ffb47c71e5149656e6f7
Timofey Privalov <timofmax@gmail.com>
2021-10-26 23:15:31 +0300

    small changes

--- Data/Network/ImageAPIService.swift
+++ Data/Network/ImageAPIService.swift
@@ -9,7 +9,6 @@ import UIKit
 
 protocol ImageAPIServiceProtocol {
     func loadImage(url: URL, handler: @escaping HandlerImage)
-    func downloadDetailedMovies(id: Int, complition: @escaping ((MovieDetailRealm)->()))
 }
 
 typealias HandlerImage = ((UIImage)->())
@@ -17,31 +16,12 @@ typealias HandlerImage = ((UIImage)->())
 final class ImageAPIService: ImageAPIServiceProtocol {
 
     func loadImage(url: URL, handler: @escaping HandlerImage) {
-        DispatchQueue.main.async {
+        DispatchQueue.global().async {
             guard let imageData = try? Data(contentsOf: url) else { return }
-            guard let image = UIImage(data: imageData) else { return }
-            handler(image)
-        }
-    }
-
-    // MARK: - Public methods
-    func downloadDetailedMovies(id: Int, complition: @escaping ((MovieDetailRealm)->())) {
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        let jsonUrlString =
-            "https://api.themoviedb.org/3/movie/\(id)?api_key=3227cbb07711665d37db3b97df155838&language=en-US"
-        guard let url = URL(string: jsonUrlString) else { return }
-        let session = URLSession.shared.dataTask(with: url) { [self] data, _, error in
-            guard let data = data else { return }
-            do {
-                let specifiMovie = try decoder.decode(MovieDetailRealm.self, from: data)
-                DispatchQueue.main.async {
-                    complition(specifiMovie)
-                }
-            } catch {
-                print("\(error)")
+            DispatchQueue.main.async {
+                guard let image = UIImage(data: imageData) else { return }
+                handler(image)
             }
         }
-        session.resume()
     }
 }
--- Data/Network/MovieAPIService.swift
+++ Data/Network/MovieAPIService.swift
@@ -18,7 +18,7 @@ final class MovieAPIService: MovieAPIServiceProtocol {
         let urlMovie =
             "https://api.themoviedb.org/3/movie/top_rated?api_key=3227cbb07711665d37db3b97df155838&language=en-US&page=3#"
         guard let url = URL(string: urlMovie) else { return }
-        URLSession.shared.dataTask(with: url) { [self] data, _, error in
+        URLSession.shared.dataTask(with: url) { data, _, error in
             guard let data = data else { return }
             do {
                 let mv = try decoder.decode(IncomingJson.self, from: data)
@@ -36,7 +36,7 @@ final class MovieAPIService: MovieAPIServiceProtocol {
         let jsonUrlString =
             "https://api.themoviedb.org/3/movie/\(id)?api_key=3227cbb07711665d37db3b97df155838&language=en-US"
         guard let url = URL(string: jsonUrlString) else { return }
-        let session = URLSession.shared.dataTask(with: url) { [self] data, _, error in
+        let session = URLSession.shared.dataTask(with: url) { data, _, error in
             guard let data = data else { return }
             do {
                 let incomingData = try decoder.decode(MovieDetailRealm.self, from: data)
